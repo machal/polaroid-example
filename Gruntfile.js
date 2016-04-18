6fd44d3a96c120621f8a0fd3f6940dcdb8891d75
Martin Michálek <michalek@shortcat.cz>
2016-04-18 09:37:48 +0200

    Grunt - start projektu z aktualniho adresare

--- Gruntfile.js
+++ Gruntfile.js
@@ -150,7 +150,7 @@ module.exports = function(grunt) {
           },
           options: {
               watchTask: true,
-              proxy: 'sites.localhost'
+              server: './'
           }
       }
     },
